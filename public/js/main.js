4bbd23705d2e21648f63d101c241ed75292fb1d5
kijimaD <norimaking777@gmail.com>
2022-09-20 16:32:20 +0900

    feat: sample model

--- public/js/main.js
+++ public/js/main.js
@@ -8,8 +8,9 @@ function init() {
   const height = 540;
 
   // レンダラーを作成
+  const canvasElement = document.querySelector('#myCanvas');
   const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#myCanvas')
+    canvas: canvasElement,
   });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(width, height);
@@ -19,23 +20,52 @@ function init() {
 
   // カメラを作成
   const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-  camera.position.set(0, 0, +1000);
+  camera.position.set(0, 0, 10);
+
+  // カメラコントローラーを作成
+  const controls = new THREE.OrbitControls(camera, canvasElement);
+  controls.target.set(0, 3, 0);
+  controls.update();
+
+  // 平行光源を作成
+  const directionalLight = new THREE.DirectionalLight(0xffffff);
+  directionalLight.position.set(1, 1, 1);
+  scene.add(directionalLight);
+  // 環境光を追加
+  const ambientLight = new THREE.AmbientLight(0x333333);
+  scene.add(ambientLight);
 
   box = add_cube(scene)
+  box.position.x = -400;
   sphere = add_sphere(scene)
+  sphere.position.y = 100;
+
+  // Collada形式のモデルデータを読み込む
+  const loader = new THREE.ColladaLoader();
+  // Colladaファイルのパスを指定
+  loader.load('./model/elf/elf.dae', (collada) => {
+    // 読み込み後に3D空間に追加
+    const model = collada.scene;
+    model.position.x = 100;
+    scene.add(model);
+  });
+  // loader.load('./model/box/box.dae', (collada) => {
+  //   // 読み込み後に3D空間に追加
+  //   const model = collada.scene;
+  //   model.position.x = 10
+  //   scene.add(model);
+  // });
 
   // 初回実行
   tick();
-  box.position.x = -400;
 
   // 毎フレーム時に実行されるループイベントです
   function tick() {
-    box.rotation.y += 0.01;
-    box.rotation.x += 0.01;
-    sphere.position.x += 1;
-    sphere.rotation.y += 0.01;
-    renderer.render(scene, camera); // レンダリング
+    // box.rotation.y += 0.01;
+    // box.rotation.x += 0.01;
+    // sphere.rotation.y += 0.01;
 
+    renderer.render(scene, camera); // レンダリング
     requestAnimationFrame(tick);
   }
 
@@ -51,11 +81,11 @@ function init() {
 
   function add_sphere(scene) {
     // 球体を作成
-    const geometry = new THREE.SphereGeometry(300, 100, 100);
+    const geometry = new THREE.SphereGeometry(30, 30, 30);
 
     // 画像を読み込む
     const loader = new THREE.TextureLoader();
-    const texture = loader.load('img/earthmap1k.jpg');
+    const texture = loader.load('./img/earthmap1k.jpg');
     // マテリアルにテクスチャーを設定
     const material = new THREE.MeshStandardMaterial({
       map: texture
@@ -65,11 +95,6 @@ function init() {
     // 3D空間にメッシュを追加
     scene.add(mesh);
 
-    // 平行光源
-    const directionalLight = new THREE.DirectionalLight(0xFFFFFF);
-    directionalLight.position.set(100, 1, 1);
-    // シーンに追加
-    scene.add(directionalLight);
     return mesh
   }
 }
