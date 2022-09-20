edf0a8397eec960edf583b5c798748f677039295
kijimaD <norimaking777@gmail.com>
2022-09-20 12:23:11 +0900

    refactor: function

--- public/js/main.js
+++ public/js/main.js
@@ -21,19 +21,47 @@ function init() {
   const camera = new THREE.PerspectiveCamera(45, width / height);
   camera.position.set(0, 0, +1000);
 
-  // 箱を作成
-  const geometry = new THREE.BoxGeometry(400, 400, 400);
-  const material = new THREE.MeshNormalMaterial();
-  const box = new THREE.Mesh(geometry, material);
-  scene.add(box);
+  box = add_cube(scene)
+  sphere = add_sphere(scene)
 
+  // 初回実行
   tick();
 
   // 毎フレーム時に実行されるループイベントです
   function tick() {
     box.rotation.y += 0.01;
+    box.rotation.x += 0.01;
+    sphere.position.x += 1;
     renderer.render(scene, camera); // レンダリング
 
     requestAnimationFrame(tick);
+  }
+
+  function add_cube(scene) {
+    // 箱を作成
+    const geometry = new THREE.BoxGeometry(400, 400, 400);
+    const material = new THREE.MeshNormalMaterial();
+    const box = new THREE.Mesh(geometry, material);
+    scene.add(box);
+
+    return box
+  }
+
+  function add_sphere(scene) {
+    // 球体を作成
+    const geometry = new THREE.SphereGeometry(300, 100, 100);
+    // マテリアルを作成
+    const material = new THREE.MeshStandardMaterial({color: 0xFF0000});
+    // メッシュを作成
+    const mesh = new THREE.Mesh(geometry, material);
+    // 3D空間にメッシュを追加
+    scene.add(mesh);
+
+    // 平行光源
+    const directionalLight = new THREE.DirectionalLight(0xFFFFFF);
+    directionalLight.position.set(1, 1, 1);
+    // シーンに追加
+    scene.add(directionalLight);
+    return mesh
   }
 }
